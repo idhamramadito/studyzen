dd05a1787c3cf711afe4b5d7caf20f7d61062032
idhamramadito <idhamadito@gmail.com>
2021-10-05 17:53:10 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Color(0xff008AD5), //Colors.blue[600],
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text('Jadwal'),
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Icon(Icons.search),
+          ),
+          Icon(Icons.more_vert),
+        ],
+      ),
+        body: SfCalendar(
+        view: CalendarView.month,
+      showNavigationArrow: true,
+      selectionDecoration: BoxDecoration(
+        color: Colors.transparent,
+            border: Border.all(color: Colors.red, width: 2),
+            borderRadius: const BorderRadius.all(Radius.circular(4)),
+            shape: BoxShape.rectangle,
+      ),
+      monthViewSettings: MonthViewSettings(showAgenda: true),
+      firstDayOfWeek: 1,
+      //initialDisplayDate: DateTime(2021, 03, 01, 08, 30),
+      //initialSelectedDate: DateTime(2021, 03, 01, 08, 30),
+      dataSource: ScheduleDataSource(getAppointments()),
+      ),
+    );
+  }
+}
+
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+  meetings.add(Appointment(
+      startTime: startTime,
+      endTime: endTime,
+      subject: 'Class',
+      color: Colors.yellow[900],
+      recurrenceRule: 'FREQ=DAILY;COUNT=10',
+      isAllDay: false));
+
+  return meetings;
+}
+
+class ScheduleDataSource extends CalendarDataSource {
+  ScheduleDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
